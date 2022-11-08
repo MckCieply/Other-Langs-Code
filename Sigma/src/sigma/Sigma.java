9a12f319df16eb7d6a172c0c45cb6d1af4041e38
Alex <mwppl123@gmail.com>
2022-11-08 13:19:14 +0100

    Crearing code

--- Sigma/src/sigma/Sigma.java
+++ Sigma/src/sigma/Sigma.java
@@ -8,14 +8,18 @@ public class Sigma {
     public static void main(String[] args) {
         int n;
         double a, x, p;
-        System.out.print("Podaj x: "); x = (new Scanner(System.in)).nextDouble();
-        System.out.print("Podaj n: "); n = (new Scanner(System.in)).nextInt();
+        System.out.print("Podaj x: ");
+        x = (new Scanner(System.in)).nextDouble();
+        System.out.print("Podaj n: ");
+        n = (new Scanner(System.in)).nextInt();
         p = 0;
         for(n = n; n > 0; n--){
-            System.out.print("Podaj a: "); a = (new Scanner(System.in)).nextDouble();
+            System.out.print("Podaj a: ");
+            a = (new Scanner(System.in)).nextDouble();
             p = (p+a)*x;
         }
-        System.out.print("Podaj ostatnie a: "); a = (new Scanner(System.in)).nextDouble();
+        System.out.print("Podaj ostatnie a: "); 
+        a = (new Scanner(System.in)).nextDouble();
         p = p + a;
         System.out.print("Wynik to: "+p+"\n");
     }

89feabd9ab5bd5e9fb62e21bf88c897b23a5d756
Alex <mwppl123@gmail.com>
2022-11-28 16:52:58 +0100

    Dodanie errora jak podany zostanie zly znak

--- StringsIndexing/src/stringsindexing/StringsIndexing.java
+++ StringsIndexing/src/stringsindexing/StringsIndexing.java
@@ -5,24 +5,33 @@ public class StringsIndexing {
     public static void main(String[] args) {
         Scanner scann = new Scanner( System.in );
 
-        char[] tabChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' ,'I', 'J', 'K', 'L', 'M', 
-                            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ' };
- 
         int[] counters = new int[ 128 ];
         for( int nr = 0; nr < counters.length; nr++) counters[ nr ] = 0;
         
         System.out.print("Podaj tekst: "); 
         String base = scann.nextLine();
-                   
+        
+        boolean error = false;
         for(int i = 0; i < base.length();i++){
-            counters[base.charAt(i)]++;
+            char chr = base.charAt(i);
+            if( chr >= '0' && chr <= '9' || chr >= 'A' && chr <= 'Z' || chr >= 'a' && chr <= 'z' || chr == ' '){
+                counters[chr]+=1;
+            }
+            else{
+                error = true;
+                break;
+            }
         }
-        for( int i = 0; i < counters.length; i++ )
-               if( counters[ i ] > 0 )
-                  System.out.println((char)i + ": " + counters[ i ] + " " );
+        if(error == true){
+            System.out.println("Somethings wrong");
+        }
+        else{
+            for(int i = 0; i < counters.length; i++){
+                   if( counters[i] > 0 ){
+                      System.out.println((char)i + ": " + counters[ i ] + " " );
+                   }
+            }
+        }
     }
     
 }

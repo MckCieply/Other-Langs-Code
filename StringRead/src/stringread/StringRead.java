04b7bf6bc11563cc0077c297d6ab0d099973521c
Alex <mwppl123@gmail.com>
2022-12-05 16:04:08 +0100

    Removing unused data

--- StringRead/src/stringread/StringRead.java
+++ StringRead/src/stringread/StringRead.java
@@ -1,25 +1,18 @@
 
 package stringread;
 import java.util.Scanner;
-import java.util.Random;
 public class StringRead {
 
     public static void main(String[] args) {
         Scanner scann = new Scanner( System.in );
 
-        char[] tabChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' ,'I', 'J', 'K', 'L', 'M', 
-                            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
- 
         String text = scann.nextLine();
         //counters innit
         int[] counters = new int[ 128 ];
         for( int nr = 0; nr < counters.length; nr++) counters[ nr ] = 0;
         
         int len = text.length();
-        
+        // +1 for all instances of letter
         for (int i=0; i<len; i++){
             counters[text.charAt(i)] += 1;
         }

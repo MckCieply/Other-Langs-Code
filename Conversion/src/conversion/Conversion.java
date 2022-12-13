678389d749f03bb952f18bd365de72b26b8e775a
Alex <mwppl123@gmail.com>
2022-12-13 10:57:12 +0100

    Final version

--- Conversion/src/conversion/Conversion.java
+++ Conversion/src/conversion/Conversion.java
@@ -1,9 +1,28 @@
 
 package conversion;
+import java.util.Scanner;
 public class Conversion {
-    
     public static void main(String[] args) {
+    Scanner scann = new Scanner(System.in);
+    String wynik = "";
     
+    System.out.print("Podaj liczbe: ");
+    int num = scann.nextInt();
+    System.out.println("Podaj podstawe liczenia (2-36)");   
+    int podstawa = scann.nextInt();
+    
+    while (num > 0){
+        int reszta = num % podstawa;
+        if (reszta > 9){
+            reszta = reszta - 10 + 'A';
+        }
+        else{
+            reszta += '0';
+        }
+        wynik = (char)reszta + wynik;
+        num /= podstawa;
+    }
+    System.out.println(wynik);
     }
     
 }

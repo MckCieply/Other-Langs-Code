6dd1817902051d40d044dc86a7f7bed987858b21
Alex <mwppl123@gmail.com>
2023-01-22 16:17:28 +0100

    reading data from file

--- RAFHomework/src/rafhomework/RAFHomework.java
+++ RAFHomework/src/rafhomework/RAFHomework.java
@@ -1,11 +1,20 @@
 
 package rafhomework;
 
+import java.io.*;
 
 public class RAFHomework {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+    RandomAccessFile raf = new RandomAccessFile("dane.txt","r");
+    char sign;
     
+    for (int i=0; i<raf.length();i++){
+        raf.seek(i);
+        sign = (char) raf.read();
+        System.out.print(sign);
+    }
+    raf.close();
     }
     
 }
